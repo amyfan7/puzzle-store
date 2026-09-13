f8dca4e22c44d4fc8c2b32c4ade1a365ce367a6e
amyfan7 <afan1@depaul.edu>
2026-09-13 02:36:45 -0500

    created spring boot project

---
+++ src/main/java/com/amyfan/puzzlestore/PuzzlestoreApplication.java
@@ -0,0 +1,13 @@
+package com.amyfan.puzzlestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzlestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuzzlestoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amyfan/puzzlestore/PuzzlestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amyfan.puzzlestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuzzlestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
